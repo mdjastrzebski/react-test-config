b23277ab1e676461fb6ca5a1dea10cb2dc5b97c8
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-12 10:43:15 +0200

    refactor: tweaks

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ export interface ConfigureOptions {
   enableOwnerStacks?: boolean;
 }
 
-export function configure(options: ConfigureOptions = {}): void {
-  if (!options.enableOwnerStacks) {
+export function configure({ enableOwnerStacks = true }: ConfigureOptions = {}): void {
+  if (!enableOwnerStacks) {
     disableOwnerStacks();
   }
 }
@@ -25,18 +25,12 @@ function disableOwnerStacks(): void {
       return;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const jestGlobal = (globalThis as any).jest;
-    if (jestGlobal != null) {
-      jestGlobal.spyOn(ReactSharedInternals, RECENTLY_CREATED_OWNER_STACKS, 'get').mockReturnValue(Infinity);
-      jestGlobal.spyOn(ReactSharedInternals, RECENTLY_CREATED_OWNER_STACKS, 'set').mockImplementation(() => {});
-    } else {
-      Object.defineProperty(ReactSharedInternals, RECENTLY_CREATED_OWNER_STACKS, {
-        get: () => Infinity,
-        set: () => {},
-        configurable: true,
-      });
-    }
+    Object.defineProperty(ReactSharedInternals, RECENTLY_CREATED_OWNER_STACKS, {
+      get: () => Infinity,
+      set: () => {},
+      configurable: true,
+      enumerable: true,
+    });
   } catch (error) {
     console.error('[react-test-config] Failed to disable owner stacks:', error);
   }
